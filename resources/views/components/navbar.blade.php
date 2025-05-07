623b61b99fe73bad1c52f31746560cf5ce348f4f
olivia3005if <oliviaputri729@gmail.com>
2025-05-07 15:13:30 +0700

    First commit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,6 +3,7 @@
         <a class="navbar-brand" href="{{ route('dashboard') }}">
             <span class="text-danger">K</span>Drama Manager
         </a>
+        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
